bcf58dbb6a291e6fc1d056f6e6739af5ced3b096
bakaq <bakaq@bakaq.net>
2023-07-08 00:05:17 -0300

    Basic UART input handling

--- src/main.rs
+++ src/main.rs
@@ -55,13 +55,50 @@ extern "C" fn abort() -> ! {
 #[no_mangle]
 extern "C" fn kmain() {
     // Init UART
-
     // Safety: This points to a valid UART device
     let uart = unsafe { uart::Uart::new(0x1000_0000) };
     uart.init();
 
     println!("Hello world!");
+    
+    loop {
+        if let Some(c) = uart.get() {
+            match c {
+                8 | 127 => {
+                    // Backspace
+                    print!("{} {}", 8 as char, 8 as char);
+                },
+                b'\n' | b'\r' => {
+                    // Newline
+                    println!();
+                },
+                0x1b => {
+                    // Escape sequence
+                    if let Some(b'[') = uart.get() {
+                        if let Some(b) = uart.get() {
+                            match b {
+                                b'A' => println!("Up arrow"),
+                                b'B' => println!("Down arrow"),
+                                b'C' => println!("Right arrow"),
+                                b'D' => println!("Left arrow"),
+                                _ => println!("Escape sequence"),
+                            }
+                        }
+                    }
+                },
+                b']' => break,
+                _ => {
+                    if c.is_ascii_alphanumeric() {
+                        print!("{}", c as char);
+                    } else {
+                        println!("Byte: {}", c);
+                    }
+                }
+            }
+        }
+    }
 
+    // Shutdown
     // Safety: This points to a valid syscon device
     unsafe { (0x100000 as *mut u16).write_volatile(0x5555) };
 }
